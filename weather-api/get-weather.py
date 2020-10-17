c76647a09a3359bb29fdcf38e47c289c1c4aba68
Chems Eddine Senoussi <chemsedd@outlook.fr>
2020-10-17 01:50:53 +0100

    ✔ Added init arg parser [weather-api]

--- weather-api/get-weather.py
+++ weather-api/get-weather.py
@@ -3,6 +3,23 @@ import argparse
 import pprint
 
 
+def init_args():
+    """Defines command line arguments.
+
+    Returns:
+        ArgumentParser: argument parser that contains the specified arguments.
+    """
+    helper = 'Retrieves the current weather data from the api.openweathremap.org server.'
+    parser = argparse.ArgumentParser(description=helper)
+
+    parser.add_argument(
+        'city', type=str, help='The name of the city to get weather for.')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='displays more weather data')
+
+    return parser
+
+
 def get_weather_data(city, *args, **kwargs):
     API_KEY = "7f470030130dcc86a0ec2e4869420ac1"
     URL = f"http://api.openweathermap.org/data/2.5/weather?"\
@@ -11,6 +28,11 @@ def get_weather_data(city, *args, **kwargs):
     print(URL)
     data = requests.get(URL).json()
     return data
+
+
+def print_weather_data(data):
+
+    pass
 
 
 if __name__ == "__main__":
